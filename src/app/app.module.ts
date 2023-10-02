f87ceb3d6439b9f3d140d9f353531362f2b654e2
priyashan <shanmugapriyashanu2002@gmail.com>
2023-10-02 13:29:09 +0530

    [BUGFIX] Removed unused imports and reverted package.json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,6 @@ import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 import { AppRoutingModule } from "./app-routing.module";
 import { ComponentsModule } from "./components/components.module";
 import { CommonModule } from "@angular/common";
-import { CampaignComponent } from './pages/campaign/campaign.component';
 
 @NgModule({
   imports: [
